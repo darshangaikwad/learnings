fde4c43079885b95d62c69a304e11a018fc65d51
darshangaikwad <darshangkwd8182@gmail.com>
2021-05-22 11:14:15 +0530

    Dark themes

---
+++ app/src/main/java/com/darshangaikwad/mylearning/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.darshangaikwad.mylearning
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Darshan Gaikwad on 17,May,2021
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
