1d0a4a83b8afd03d4062348502da024a5baaab17
darshangaikwad <darshangkwd8182@gmail.com>
2021-05-24 23:05:03 +0530

    Recycle view example with image and text view.

--- app/src/main/java/com/darshangaikwad/mylearning/model/Affirmation.kt
+++ app/src/main/java/com/darshangaikwad/mylearning/model/Affirmation.kt
@@ -1,6 +1,9 @@
 package com.darshangaikwad.mylearning.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 /**
  * Created by Darshan Gaikwad on 24,May,2021
  */
-data class Affirmation(val stringResourceId : Int)
+data class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int)
