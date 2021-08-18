17dbfbfb4ce4be5f25ce994fdedf59e2edb97b80
darshangaikwad <darshangkwd8182@gmail.com>
2021-08-18 09:48:31 +0530

    Basic code setup for cupcake application

---
+++ app/src/main/java/com/darshangaikwad/mylearning/cupcake/CupcakeActivity.kt
@@ -0,0 +1,9 @@
+package com.darshangaikwad.mylearning.cupcake
+
+import androidx.appcompat.app.AppCompatActivity
+import com.darshangaikwad.mylearning.R
+
+/**
+ * Created by Darshan Gaikwad on 17,August,2021
+ */
+class CupcakeActivity : AppCompatActivity(R.layout.activity_cupcake)
