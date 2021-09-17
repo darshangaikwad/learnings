9b4f5575af93e33dc62396aebe5844cb7e687c12
darshangaikwad <darshangkwd8182@gmail.com>
2021-09-17 12:19:41 +0530

    Complete cupcake ordering application
    Signed-off-by: darshangaikwad <darshangkwd8182@gmail.com>

--- app/src/main/java/com/darshangaikwad/mylearning/cupcake/CupcakeActivity.kt
+++ app/src/main/java/com/darshangaikwad/mylearning/cupcake/CupcakeActivity.kt
@@ -2,6 +2,7 @@ package com.darshangaikwad.mylearning.cupcake
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.darshangaikwad.mylearning.R
@@ -11,11 +12,17 @@ import com.darshangaikwad.mylearning.R
  */
 class CupcakeActivity : AppCompatActivity(R.layout.activity_cupcake) {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.cupcake_nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/darshangaikwad/mylearning/cupcake/view/SummaryFragment.kt
+++ app/src/main/java/com/darshangaikwad/mylearning/cupcake/view/SummaryFragment.kt
@@ -1,5 +1,6 @@
 package com.darshangaikwad.mylearning.cupcake.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.darshangaikwad.mylearning.R
 import com.darshangaikwad.mylearning.cupcake.viewmodel.OrderViewModel
 import com.darshangaikwad.mylearning.databinding.FragmentSummaryBinding
 
@@ -44,7 +47,23 @@ class SummaryFragment : Fragment() {
      * Submit the order by sharing out the order details to another app via an implicit intent.
      */
     fun sendOrder() {
-        Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
+        val numberOfCupcakes = sharedViewModel.quantity.value ?: 0
+        val orderSummary = getString(
+            R.string.order_details,
+            resources.getQuantityString(R.plurals.cupcakes, numberOfCupcakes, numberOfCupcakes),
+            sharedViewModel.flavor.value.toString(),
+            sharedViewModel.date.value.toString(),
+            sharedViewModel.price.value.toString()
+        )
+
+        val intent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.new_cupcake_order))
+            .putExtra(Intent.EXTRA_TEXT, orderSummary)
+
+        if (activity?.packageManager?.resolveActivity(intent, 0) != null) {
+            startActivity(intent)
+        }
     }
 
     /**
@@ -54,5 +73,10 @@ class SummaryFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
     }
 }
